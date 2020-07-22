66d2051becb3a21551c5b5c3f882abfe1662eefe
cbb <learnr_27@126.com>
2020-07-22 11:56:30 +0800

    sql

---
+++ seata.sql
@@ -0,0 +1,84 @@
+CREATE TABLE IF NOT EXISTS seata_amount.`undo_log`
+(
+    `branch_id`     BIGINT(20)   NOT NULL COMMENT 'branch transaction id',
+    `xid`           VARCHAR(100) NOT NULL COMMENT 'global transaction id',
+    `context`       VARCHAR(128) NOT NULL COMMENT 'undo_log context,such as serialization',
+    `rollback_info` LONGBLOB     NOT NULL COMMENT 'rollback info',
+    `log_status`    INT(11)      NOT NULL COMMENT '0:normal status,1:defense status',
+    `log_created`   DATETIME(6)  NOT NULL COMMENT 'create datetime',
+    `log_modified`  DATETIME(6)  NOT NULL COMMENT 'modify datetime',
+    UNIQUE KEY `ux_undo_log` (`xid`, `branch_id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8 COMMENT ='AT transaction mode undo table';
+
+DROP TABLE IF EXISTS seata_amount.account;
+
+CREATE TABLE seata_amount.account
+(
+    id               INT(11) NOT NULL AUTO_INCREMENT,
+    balance          DOUBLE   DEFAULT NULL,
+    last_update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB AUTO_INCREMENT = 1  DEFAULT CHARSET = utf8;
+
+
+INSERT INTO seata_amount.account (id, balance) VALUES (1, 100);
+SELECT * FROM seata_amount.account;
+
+CREATE TABLE IF NOT EXISTS seata_order.`undo_log`
+(
+    `branch_id`     BIGINT(20)   NOT NULL COMMENT 'branch transaction id',
+    `xid`           VARCHAR(100) NOT NULL COMMENT 'global transaction id',
+    `context`       VARCHAR(128) NOT NULL COMMENT 'undo_log context,such as serialization',
+    `rollback_info` LONGBLOB     NOT NULL COMMENT 'rollback info',
+    `log_status`    INT(11)      NOT NULL COMMENT '0:normal status,1:defense status',
+    `log_created`   DATETIME(6)  NOT NULL COMMENT 'create datetime',
+    `log_modified`  DATETIME(6)  NOT NULL COMMENT 'modify datetime',
+    UNIQUE KEY `ux_undo_log` (`xid`, `branch_id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8 COMMENT ='AT transaction mode undo table';
+
+DROP TABLE IF EXISTS seata_order.orders;
+
+CREATE TABLE seata_order.orders
+(
+    id               INT(11) NOT NULL AUTO_INCREMENT,
+    user_id          INT(11)        DEFAULT NULL,
+    product_id       INT(11)        DEFAULT NULL,
+    pay_amount       DECIMAL(10, 0) DEFAULT NULL,
+    add_time         DATETIME       DEFAULT CURRENT_TIMESTAMP,
+    last_update_time DATETIME       DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8;
+
+SELECT * FROM seata_order.orders;
+
+CREATE TABLE IF NOT EXISTS seata_order.`undo_log`
+(
+    `branch_id`     BIGINT(20)   NOT NULL COMMENT 'branch transaction id',
+    `xid`           VARCHAR(100) NOT NULL COMMENT 'global transaction id',
+    `context`       VARCHAR(128) NOT NULL COMMENT 'undo_log context,such as serialization',
+    `rollback_info` LONGBLOB     NOT NULL COMMENT 'rollback info',
+    `log_status`    INT(11)      NOT NULL COMMENT '0:normal status,1:defense status',
+    `log_created`   DATETIME(6)  NOT NULL COMMENT 'create datetime',
+    `log_modified`  DATETIME(6)  NOT NULL COMMENT 'modify datetime',
+    UNIQUE KEY `ux_undo_log` (`xid`, `branch_id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8 COMMENT ='AT transaction mode undo table';
+
+DROP TABLE IF EXISTS seata_order.product;
+
+CREATE TABLE seata_order.product
+(
+    id               INT(11) NOT NULL AUTO_INCREMENT,
+    stock            INT(11)  DEFAULT NULL,
+    last_update_time DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8;
+
+INSERT INTO seata_order.product (id, stock) VALUES (1, 10);
+
+SELECT * FROM seata_order.product;
